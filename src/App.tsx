d6b5d115eeba983a8fa19e27116d05021bd24a8f
Morné Louw <louw.morne@gmail.com>
2022-03-29 15:39:08 +0200

    Describing (Typing) function arguments

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import Todos from "./components/Todos";
 
 function App() {
+    const items = ["Item One", "Item Two"];
     return (
         <div>
-            <Todos />
+            <Todos items={items} />
         </div>
     );
 }
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -5,8 +5,14 @@ import React from "react";
 // React.FC is also a generic type similar to an Array.
 // - An Array needs to be given the type of the data it will hold, eg. Array<string> or Array<string | number>
 // - In the same way React.FC can be told what data it can receive, eg. React.FC<{foo: string}>
-const Todos: React.FC = (props) => {
-    return <ul>{}</ul>;
+const Todos: React.FC<{ items: string[] }> = (props) => {
+    return (
+        <ul>
+            {props.items.map((item) => (
+                <li>{item}</li>
+            ))}
+        </ul>
+    );
 };
 
 export default Todos;
